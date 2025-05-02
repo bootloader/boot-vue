c6c65ef2c2756802bfb78902535db4ec082a2fa8
Lalit Tanwar | The Architect <lalit.tanwar07@gmail.com>
2025-05-03 00:00:35 +0530

    Update AppConfig.js

--- src/@common/AppConfig.js
+++ src/@common/AppConfig.js
@@ -14,7 +14,7 @@ function getlocalConfig() {
 
 let CONFIG = {
   getAppName() {
-    let slectedAppName = window.CONST?.APP;
+    let slectedAppName = window.CONST?.WEBAPP || window.CONST?.APP;
     if (!slectedAppName) {
       for (let key in this.apps) {
         let app = this.apps[key];
